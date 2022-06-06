2a50224dee61b62fa1cc08fc43da41316929da9f
ssumthingood <ssumthingood@gmail.com>
2022-06-06 20:27:30 +0900

    practicing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  content: [
-    './pages/**/*.{tsx,ts,js,jsx}',
-    './components/**/*.{tsx,ts,js,jsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./pages/**/*.{tsx,ts,js,jsx}", "./components/**/*.{tsx,ts,js,jsx}"],
+    theme: {
+        extend: {},
+    },
+    darkMode: "media", // class
+    plugins: [],
 };
