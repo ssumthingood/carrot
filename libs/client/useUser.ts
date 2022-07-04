df091a02884469b174fb21d286f8a4997ed1eea9
ssumthingood <ssumthingood@gmail.com>
2022-07-05 00:13:22 +0900

    0705 auth

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,24 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function useUser() {
-    const [user, setUser] = useState();
+    const { data, error } = useSWR("/api/users/me");
+    // const [user, setUser] = useState();
     const router = useRouter();
     useEffect(() => {
-        fetch("/api/users/me")
-            .then((res) => res.json())
-            .then((data) => {
-                if (!data.ok) {
-                    return router.replace("/enter");
-                }
-                setUser(data.profile);
-            });
-    }, [router]);
-    return user;
+        if (data && !data.ok) router.replace("/enter");
+    }, [data, router]);
+    // useEffect(() => {
+    //     fetch("/api/users/me")
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             if (!data.ok) {
+    //                 return router.replace("/enter");
+    //             }
+    //             setUser(data.profile);
+    //         });
+    // }, [router]);
+    // return user;
+    return { user: data?.profile, isLoading: !data && !error };
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true, //false로 할 시 개발자 모드에서 페이지 로딩시 나오는 console.log()등등에서 정보가 두번씩 나오지 않음
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
